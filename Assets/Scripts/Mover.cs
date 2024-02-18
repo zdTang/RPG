31df9d7f4679eeaacf037dbfca93fa3df6c99c08
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-02-18 14:32:49 -0500

    Camera can cast ray when mouse click the terrain

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,9 +6,14 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
         [SerializeField] Transform target;
+        Ray lastRay;
     
         private void Update()
         {
+            if(Input.GetMouseButtonDown(0)){
+                lastRay=Camera.main.ScreenPointToRay(Input.mousePosition);
+            }
+            Debug.DrawRay(lastRay.origin, lastRay.direction*100,Color.red);
             GetComponent<NavMeshAgent>().destination=target.position;
         }
 
