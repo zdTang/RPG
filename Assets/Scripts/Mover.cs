5fe2c55750e35088cd1955f9e48c619c4ad069fc
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-02-18 12:37:48 -0500

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+        [SerializeField] Transform target;
+    
+        private void Update()
+        {
+            GetComponent<NavMeshAgent>().destination=target.position;
+        }
+
+      
+    
+}
