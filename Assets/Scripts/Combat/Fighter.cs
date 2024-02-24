b234c2063f81527fb9e885d63c973ab9dca65da6
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-02-24 11:44:16 -0500

    trigger animation in code

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -21,7 +21,13 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
+        }
+
+        private void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         private bool GetIsInRange()
