a8ca6eb7f14f9994cf0e0a48b857b42a0c471f0f
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-02-24 10:10:56 -0500

    decoupling dependency cycles
    1. a very good example of design pattern
    to change between two actions

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,4 +1,5 @@
 ﻿using RPG.Combat;
+using RPG.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,6 +25,7 @@ namespace RPG.Movement
 
         public void StartMoveAction(Vector3 destination)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             GetComponent<Fighter>().Cancel();
             MoveTo(destination);
         }
